6040eb8a525f166c12b66b405dd8c3b7a830ac49
Rabindra Tamang <rabee.tmg123@gmail.com>
2022-07-02 22:01:48 +0545

    console app for storing basic student details

---
+++ StudentApp/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.IO;
+using System.Text.Json;
+using StudentApp;
+
+
+
+
+
+
+
+StudentMethod studentmethod = new StudentMethod();
+List<StudentModel> students = new List<StudentModel>();
+
+
+int option;
+
+do
+{
+    Console.WriteLine("1. Enter the student Info ");
+    Console.WriteLine("2. Update the student Info");
+    Console.WriteLine("3. Delete the student Info");
+    Console.WriteLine("4. Display all the student Info");
+    Console.Write("Choose and option from above:");
+    option = int.Parse(Console.ReadLine());
+    switch (option)
+    {
+        case 1:
+            students = studentmethod.RegisterStudent(students);
+            string jsonString = JsonSerializer.Serialize(students, new JsonSerializerOptions() { WriteIndented = true });
+            using (StreamWriter saveFile = new StreamWriter(@"E:\Csharp\StudentInfo.json"))
+            {
+                saveFile.WriteLine(jsonString);
+            }
+            break;
+
+        case 2:
+            students = studentmethod.UpdateStudent(students);
+            string updatestring = JsonSerializer.Serialize(students, new JsonSerializerOptions() { WriteIndented = true });
+            using (StreamWriter updateFile = new StreamWriter(@"E:\Csharp\StudentInfo.json"))
+            {
+                updateFile.WriteLine(updatestring);
+            }
+            break;
+
+        case 3:
+            students = studentmethod.DeleteStudent(students);
+            string deletestudent = JsonSerializer.Serialize(students, new JsonSerializerOptions() { WriteIndented = true });
+            using (StreamWriter updateFile = new StreamWriter(@"E:\Csharp\StudentInfo.json"))
+            {
+                updateFile.WriteLine(deletestudent);
+            }
+            
+            break;
+
+        case 4:
+            studentmethod.GetStudentDetails(students);
+            break;
+
+    }
+
+}
+while (option > 0 && option < 5);
+
---
+++ StudentApp/StudentMethod.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentApp
+{
+    internal class StudentMethod
+    {
+        public void GetStudentDetails(List<StudentModel> studentslist)
+        {
+            foreach (StudentModel student in studentslist)
+            {
+                Console.WriteLine(student.Name);
+            }
+        }
+
+        public List<StudentModel> RegisterStudent(List<StudentModel> students)
+        {
+            StudentModel studentmodel = new StudentModel();
+            Console.Write("Enter student id: ");
+            studentmodel.Id = int.Parse(Console.ReadLine());
+            Console.Write("Enter student name: ");
+            studentmodel.Name = Console.ReadLine();
+            Console.Write("Enter student address: ");
+            studentmodel.Address = Console.ReadLine();
+            students.Add(studentmodel);
+            Console.WriteLine("Student with given info recored successfully");
+            return students;
+
+
+        }
+
+        public List<StudentModel> DeleteStudent(List<StudentModel> studentslist)
+        {
+            int count = 0;
+            Console.Write("Enter student id to delete: ");
+            int id = int.Parse(Console.ReadLine());
+            foreach (StudentModel student in studentslist)
+            {
+                if (id == student.Id)
+                {
+                    studentslist.Remove(student);
+                    Console.WriteLine("Input id deleted sucessfully");
+                    count = count + 1;
+                    return studentslist;
+
+                }
+                
+
+
+            }
+            if (count==0)
+            {
+                Console.WriteLine("ID not found");
+            }
+            return null;
+        }
+        public List<StudentModel> UpdateStudent(List<StudentModel> studentslist)
+        {
+            int count = 0;
+            Console.Write("Enter student id to update: ");
+            int id = int.Parse(Console.ReadLine());
+            foreach(StudentModel student in studentslist)
+            {
+                if(id == student.Id)
+                {
+                    Console.Write("Enter student name: ");
+                    student.Name = Console.ReadLine();
+                    Console.Write("Enter student address: ");
+                    student.Address = Console.ReadLine();
+                    Console.WriteLine("Updated successfully");
+                    count = count + 1;
+                    return studentslist;
+
+                }
+               
+
+            }
+            if(count == 0)
+            {
+                Console.WriteLine("Id not found");
+            }
+            return null;
+        }
+    }
+}
